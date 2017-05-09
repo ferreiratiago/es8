302bc481de6919e05982ed84e7b97b0d35e4b6d1
Tiago Ferreira <ferreiratiago@outlook.com>
2017-05-09 17:43:32 +0100

    Play with getOwnPropertyDescriptors

--- object.js
+++ object.js
@@ -10,3 +10,17 @@ Object.values(obj)  // [ 'Mr.Foo', 'Mr.Bar' ]
 // Object.entries
 // Returns a pair [key , value] for each element in the object.
 Object.entries(obj) // [ [ 'foo', 'Mr.Foo' ], [ 'bar', 'Mr.Bar' ] ]
+
+// Object.getOwnPropertyDescriptors
+// Returns all object properties descriptors.
+console.log(Object.getOwnPropertyDescriptors(obj))
+// { foo:
+//    { value: 'Mr.Foo',
+//      writable: true,
+//      enumerable: true,
+//      configurable: true },
+//   bar:
+//    { value: 'Mr.Bar',
+//      writable: true,
+//      enumerable: true,
+//      configurable: true } }
